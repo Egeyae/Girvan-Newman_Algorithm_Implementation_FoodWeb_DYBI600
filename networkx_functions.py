9b1396aa755a5c0cf3e8ef70793feede537f9f40
EllynaSoumelis <ellyna@gmx.fr>
2026-05-25 15:47:05 +0200

    networkx communities

--- networkx_functions.py
+++ networkx_functions.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import itertools
 from girvannewman import _modularity
+from girvannewman import _communities
 from girvannewman import girvannewman
 from networkx.algorithms.community.quality import modularity
 import matplotlib.pyplot as plt
@@ -38,6 +39,42 @@ def to_networkx(g):
         for u in g.get_neighborhood(v):
             g_nx.add_edge(v, u)
     return g_nx
+
+
+def comparison_communities(g, k):
+    g_nx = to_networkx(g)
+    ntx_partition = bis_communities(g_nx, k)
+    best_partition_com = _communities(g,k, return_graph= False)
+
+    in_communities = {}
+
+    for i, community in enumerate(best_partition_com):
+        for vertex in community:
+            in_communities[str(vertex)] = i
+    
+    in_communities_ntx = {}
+
+    for i, community in enumerate(ntx_partition):
+        for vertex in community:
+            in_communities_ntx[str(vertex)] = i
+
+    resemblance = 0
+    differences = 0
+    total = len(in_communities)
+
+    for vertex in in_communities:
+        com_mod = {v for v in in_communities if in_communities[v] == in_communities[vertex]}
+        com_nx = {v for v in in_communities_ntx if in_communities_ntx[v] == in_communities_ntx[vertex]}
+
+        if com_mod == com_nx :
+            resemblance +=1
+        else:
+            differences +=1
+    
+    total_resemblance = (resemblance/ total)*100
+
+    return total_resemblance, resemblance, differences
+
 
 def comparison_modularit(g):
     g_nx = to_networkx(g)
@@ -77,7 +114,7 @@ def comparison_modularit(g):
 from loader import load_karate
 g, groups = load_karate()
 
-print(comparison_modularit(g))
+print(comparison_communities(g, 2))
 
 
 def bis_dendogram(g):
