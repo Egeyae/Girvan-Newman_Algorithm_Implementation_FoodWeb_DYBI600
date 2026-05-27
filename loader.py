d4c45c89aa466b36affb727f42d1dcd9cfaed8bc
Lysa Percevault <144883672+Kerserliss@users.noreply.github.com>
2026-05-27 16:28:48 +0200

    change name

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 import Graph as g
 import csv 
 
-name_food_web = {0 : "Phytoplancton", 1 : "Suspended Bacteria",2 : "Sediment Bacteria", 3 : "Benthic diatoms", 4 : "Free bacteria", 5 : "Heterotrophic microflagellates", 6 : "Microzooplankton",7: "Zooplankton", 8: "Cnetophore", 9: "Sea nettle", 10 : "Other suspendfeeders", 11 : "Mya", 12 :"Oysters", 13 : "Other polychaetes", 14 : "Nereis", 15: "Macoma spp.", 16 : "Meio Fauna", 17 :"Crusta deposit feeders", 18: "Blue Crab", 19 :" Fish Larvae", 20 :"Alewife and Blue herring", 21 : "Bay anchovy", 22 :"Men haden", 23 :"Shad", 24:"croaker", 25 :"Hog choker", 26 : "Spot", 27 :"White Perch", 28 :"Catfish", 29 :"Blue Fish", 30 : "Weak Fish", 31 :"Summer Flounder", 32 :"Striped bass", 33 : "DOC", 34 :"POC suspended", 35 : "POC sediment"}
+name_food_web = {0 : "Phytoplancton", 1 : "Suspended Bacteria",2 : "Sediment Bacteria", 3 : "Benthic diatoms", 4 : "Free bacteria", 5 : "Heterotrophic microflagellates", 6 : "Microzooplankton",7: "Zooplankton", 8: "Cnetophore", 9: "Sea nettle", 10 : "Other suspendfeeders", 11 : "Mya", 12 :"Oysters", 13 : "Other polychaetes", 14 : "Nereis", 15: "Macoma spp", 16 : "Meio Fauna", 17 :"Crusta deposit feeders", 18: "Blue Crab", 19 :" Fish Larvae", 20 :"Alewife and Blue herring", 21 : "Bay anchovy", 22 :"Men haden", 23 :"Shad", 24:"Croaker", 25 :"Hog choker", 26 : "Spot", 27 :"White Perch", 28 :"Catfish", 29 :"Blue Fish", 30 : "Weak Fish", 31 :"Summer Flounder", 32 :"Striped bass", 33 : "DOC", 34 :"POC suspended", 35 : "POC sediment"}
 
 def load_karate():
     """
@@ -111,6 +111,7 @@ if __name__ == "__main__":
     college_graph, groups2 = load_college_football()
     foodweb_graph = load_data_food_web_mat1()
     foodweb_graph.plot()
+    print(foodweb_graph)
     #print(groups2)
     #college_graph.plot()
     #karate_graph.plot(labels=True)
